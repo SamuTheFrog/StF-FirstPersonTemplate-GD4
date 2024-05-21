322fd1b3364427bef19440250f5bab4d317eaab4
SamuTheFrog <142645037+SamuTheFrog@users.noreply.github.com>
2024-05-21 17:17:22 -0500

    Update_0.0.6
    Patch-Fix 0.0.6
    Previous version, 0.0.3, fucking broke everything.
    Head bob, colliders, framerate, physics, interpolation, everything. Crouching for some reason was a massive pain in the ass to implement. Nearly every implementation of it broke the entire movement script. 
    However, I finally figured it out, 3 versions/revisions later. 
    
    While crouched the player can no longer sprint or dodge, however jumping is still possible, and crouching while mid-air is also possible. 
    Crouching also slows player and toggles a hitbox that allows to go beneath physics areas - a different hibox entirely will be used for hit detection and environmental damage.
    ALL movement is linearly interpolated - crouching, standing, sprinting, dodging, it's all very smooth and feels weighty and realistic, but still responsive.
    Physics once again travels, that broke with the last update.
    Finally, the game knows if something is above your head and won't allow you to uncrouch if something is, preventing overhead collision bugs
    
    Everything has been built as modular as possible, so implementation of health and energy systems and having them effect the player's movement and abilities should be an easy task.
    Script is easily readable, using comments where it might not be.
    
    At last, we have a completed player movement script. May yet still receive updates as development progresses.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,124 +1,111 @@
 extends CharacterBody3D
 
-const WALK_SPEED = 9.0 # Obvious
-const MOVE_SMOOTH = 9.0 # This one is used for linear interpolation of movement slowdowns and speed ups
-const SPRINT_SPEED = 15.0 # You
-const CROUCH_SLOWDOWN = 6.0 # Absolute
-const CROUCH_AMP = 0.9 # Stupid-
-const CROUCH_SMOOTH = 9 # Okay, maybe not this one... It's the value for crouch-state enter/exit
-const STANDING_HEIGHT = 0.6# -SMOOTH BRAINED
-const JUMP_VELOCITY = 9.69 # Fucking
-const LOOK_SENSITIVITY = 0.0016 # Idiot
-const BOB_FREQ = 2.0 # Head-bob frequency
-const BOB_AMP = 0.06 # Head-bob amplitude
-const FOV_BASE = 80.0 # Field of View base
-const FOV_WARP = 0.3 # Field of view motion warping strength
-const DODGE_AMP = 900.0 # Dodge amplitude
-const DODGE_SMOOTH = 6.0 # Dodge dodge smoothing
-const DODGE_COOLDOWN = 0.6 # Dodge cooldown
-const AFTERJUMP_AMP = 15.0 # Afterjump amplitude
+const WALK_SPEED_BASE = 9.0
+const SPRINT_SPEED_AMP = 6.0
+const CROUCH_SPEED_AMP = 6.0
+const STANDING_HEIGHT = 0.6
+const CROUCH_HEIGHT_AMP = 0.9
+const DODGE_AMP = 900.0
+const DODGE_COOLDOWN = 0.6
+const JUMP_AMP = 9.69
+const DOUBLEJUMP_AMP = 15.0
+const LOOK_SENSITIVITY = .0016
+const BOB_FREQ = 2.0
+const BOB_AMP = 0.06
+const FOV_BASE = 80.0
+const FOV_WARP = 0.3
 
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity") # Gravity, this adds it, dumbass.
+var movement_speed = 6.0
+var can_dodge = true
+var can_doublejump = true
+var can_sprint = true
+var hbob = 0.0 # Not entirely sure what this does... but everything breaks without it.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-@onready var head = $head # Creates reference to head
-@onready var camera = $head/Camera3D # Creates reference to camera
-@onready var fov = "fov" # Makes FOV a publicably editable value
-@onready var standing_collider =  $standing_collider
-@onready var crouch_collider = $crouch_collider
-@onready var crouch_check_raycast = $crouch_check_raycast
+@onready var head = $head
+@onready var camera = $head/Camera3D
+@onready var fov = "fov"
+@onready var collider_standing = $collider_standing
+@onready var collider_crouched = $collider_crouched
+@onready var overhead_raycast = $overhead_raycast
 
 func _ready():
-
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) # Track mouse movement + Lock in window
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _unhandled_input(event):
-
-# Makes player look around with mouse
 	if event is InputEventMouseMotion:
 		head.rotate_y(event.relative.x * -LOOK_SENSITIVITY)
 		camera.rotate_x(event.relative.y * -LOOK_SENSITIVITY)
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-85), deg_to_rad(90))
 
 func _physics_process(delta):
-
-	var move_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-	var move_direction = (head.transform.basis * Vector3(move_dir.x, 0, move_dir.y)).normalized()
-	var movement_speed = 6.0 # Temporary value for adjusting movement speeds on the go
-	var is_crouched = false
-	var can_dodge = true # Temporary value to prevent Afterjump spam
-	var can_afterjump = true # Temporary value to prevent Temporal Jump spam
-	var hbob = 0.0 # Not entirely sure what this does... something to do with head bobbin'... BUT AS TODD HOWARD SAYS
-  # # # # # # # # # # # # # # IT # # # # # # # # # # JUST # # # # # # # # # # WORKS # # # # # # # # # # # # # # # # 
-
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	if Input.is_action_pressed("move_crouch"):
-		movement_speed = lerp(movement_speed, WALK_SPEED - CROUCH_SLOWDOWN, delta * CROUCH_SMOOTH)
-		head.position.y = lerp(head.position.y, STANDING_HEIGHT - CROUCH_AMP, delta * CROUCH_SMOOTH)
-		standing_collider.disabled = true
-		crouch_collider.disabled = false
-		can_dodge = false
-		is_crouched = true
-	else:
-		if not crouch_check_raycast.is_colliding():
-			head.position.y = lerp(head.position.y, STANDING_HEIGHT, delta * CROUCH_SMOOTH)
-			movement_speed = lerp(movement_speed, WALK_SPEED, delta * CROUCH_SMOOTH)
-			standing_collider.disabled = false
-			crouch_collider.disabled = true
-			can_dodge = true
-			is_crouched = false
-		# FAST AS FUCK BOIIIIIIIIII
-			if Input.is_action_pressed("move_sprint"):
-				movement_speed = SPRINT_SPEED
-			else:
-				movement_speed = WALK_SPEED
-		# So with this, we can dodge things now. That's cool.
-			if Input.is_action_just_pressed("move_dodge"):
-				if can_dodge:
-					velocity.x = lerp(velocity.x, move_direction.x * DODGE_AMP, delta * DODGE_SMOOTH)
-					velocity.z = lerp(velocity.z, move_direction.z * DODGE_AMP, delta * DODGE_SMOOTH)
-					can_dodge = false
-					await get_tree().create_timer(DODGE_COOLDOWN).timeout
-					can_dodge = true
-
-	# If they on the floor, they can run, lol.
-	if is_on_floor():
-		if move_direction:
-			velocity.x = lerp(velocity.x, move_direction.x * movement_speed, delta * MOVE_SMOOTH)
-			velocity.z = lerp(velocity.z, move_direction.z * movement_speed, delta * MOVE_SMOOTH)
+	if Input.is_action_pressed("move_sprint"):
+		if can_sprint:
+			movement_speed = lerp(movement_speed, WALK_SPEED_BASE + SPRINT_SPEED_AMP, delta * 6)
 		else:
-			velocity.x = lerp(velocity.x, move_direction.x * movement_speed, delta * MOVE_SMOOTH)
-			velocity.z = lerp(velocity.z, move_direction.z * movement_speed, delta * MOVE_SMOOTH)
+			movement_speed = lerp(movement_speed, WALK_SPEED_BASE, delta * 6)
+
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
+	if Input.is_action_just_pressed("move_dodge"):
+		if can_dodge:
+			velocity.x = lerp(velocity.x, direction.x * DODGE_AMP, delta * 9)
+			velocity.z = lerp(velocity.z, direction.z * DODGE_AMP, delta * 9)
+			can_dodge = false
+			await get_tree().create_timer(DODGE_COOLDOWN).timeout
+			can_dodge = true
+
+	if Input.is_action_pressed("move_crouch"):
+		movement_speed = lerp(movement_speed, WALK_SPEED_BASE - CROUCH_SPEED_AMP, delta * 3)
+		can_dodge = false
+		can_sprint = false
+		head.position.y = lerp(head.position.y, STANDING_HEIGHT - CROUCH_HEIGHT_AMP, delta * 9)
+		collider_standing.disabled = true
+		collider_crouched.disabled = false
 	else:
-		velocity.x = lerp(velocity.x, move_direction.x * movement_speed, delta * MOVE_SMOOTH)
-		velocity.z = lerp(velocity.z, move_direction.z * movement_speed, delta * MOVE_SMOOTH)
+		if not overhead_raycast.is_colliding():
+			movement_speed = lerp(movement_speed, WALK_SPEED_BASE, delta * 9)
+			can_dodge = true
+			can_sprint = true
+			head.position.y = lerp(head.position.y, STANDING_HEIGHT, delta * 3)
+			collider_crouched.disabled = true
+			collider_standing.disabled = false
 
-	# Bunny hops FTW
-	if Input.is_action_pressed("move_jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# I always liked double jumps in games... Metroid did it best.
-	if Input.is_action_just_pressed("move_jump") and not is_on_floor():
-		if can_afterjump:
-			velocity.y = AFTERJUMP_AMP
-			can_afterjump = false
-	# Prevents infinite Afterjump
 	if is_on_floor():
-		can_afterjump = true
-		
-	# Makes head go bobbin'.
+		if direction:
+			velocity.x = lerp(velocity.x, direction.x * movement_speed, delta * 9.0)
+			velocity.z = lerp(velocity.z, direction.z * movement_speed, delta * 9.0)
+		else:
+			velocity.x = lerp(velocity.x, direction.x * movement_speed, delta * 6.0)
+			velocity.z = lerp(velocity.z, direction.z * movement_speed, delta * 6.0)
+	else:
+		velocity.x = lerp(velocity.x, direction.x * movement_speed, delta * 6.0)
+		velocity.z = lerp(velocity.z, direction.z * movement_speed, delta * 6.0)
+
+	if Input.is_action_pressed("move_jump") and is_on_floor():
+		velocity.y = JUMP_AMP
+
+	if Input.is_action_just_pressed("move_jump") and not is_on_floor():
+		if can_doublejump:
+			velocity.y = DOUBLEJUMP_AMP
+			can_doublejump = false
+
+	if is_on_floor():
+		can_doublejump = true
+
 	hbob += delta * velocity.length()  * float(is_on_floor())
 	camera.transform.origin = _headbob(hbob)
-	
-	# Makes FOV warp depending on speed, but only to a max.
-	var velocity_clamped = clamp(velocity.length(), 0.9, SPRINT_SPEED * 33)
+
+	var velocity_clamped = clamp(velocity.length(), 0.9, SPRINT_SPEED_AMP * 33)
 	var target_fov = FOV_BASE + FOV_WARP * velocity_clamped
 	camera.fov = lerp(camera.fov, target_fov, delta * 9)
 
-	move_and_slide() # Oooooooooo, slippery
+	move_and_slide()
 
-# HEAD BOB FUNCIONALITY - UNIMPORTANT REALLY SO KEEP IT AT THE BOTTOM YOU FUCKING LOSER
 func _headbob(time) -> Vector3:
 	var pos = Vector3.ZERO
 	pos.y = sin(time * BOB_FREQ) * BOB_AMP
