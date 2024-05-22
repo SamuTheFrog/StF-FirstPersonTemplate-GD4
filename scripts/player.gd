47706d7ddd3c4b9257359f8823035d172fc08716
SamuTheFrog <142645037+SamuTheFrog@users.noreply.github.com>
2024-05-22 03:09:32 -0500

    Update_0.0.9
    Patch-Fix Update_0.0.9
    Fixed infinite dodging bug
    Had to restructure movement base, may create new bugs. All seems fine, so far.
    
    Far as I can tell, Update_0.0.9 is 100% bug free and ready for next phase of template development.

--- scripts/player.gd
+++ scripts/player.gd
@@ -10,15 +10,17 @@ const DODGE_COOLDOWN = 0.6
 const JUMP_AMP = 9.69
 const DOUBLEJUMP_AMP = 15.0
 const LOOK_SENSITIVITY = .0016
-const BOB_FREQ = 2.0
+const CONTROLLER_SENSITIVITY = 0.03
+const BOB_FREQ = 2.6
 const BOB_AMP = 0.06
 const FOV_BASE = 80.0
 const FOV_WARP = 0.3
 
 var movement_speed = 6.0
-var can_dodge = true
 var can_doublejump = true
 var can_sprint = true
+var can_dodge = true
+var is_crouched = false
 var hbob = 0.0 # Not entirely sure what this does... but everything breaks without it.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
@@ -33,58 +35,56 @@ func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _unhandled_input(event):
+
 	if event is InputEventMouseMotion:
 		head.rotate_y(event.relative.x * -LOOK_SENSITIVITY)
 		camera.rotate_x(event.relative.y * -LOOK_SENSITIVITY)
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-85), deg_to_rad(90))
 
 func _physics_process(delta):
+
+	var look_direction = Input.get_vector("look_left", "look_right", "look_up", "look_down")
+	var move_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var movement_direction = (head.transform.basis * Vector3(move_dir.x, 0, move_dir.y)).normalized()
+	
+	if look_direction:
+		head.rotate_y (look_direction.x * -CONTROLLER_SENSITIVITY)
+		camera.rotate_x (look_direction.y * -CONTROLLER_SENSITIVITY)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-85), deg_to_rad(90))
+
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	if Input.is_action_pressed("move_sprint"):
+	if Input.is_action_pressed("move_sprint") and not is_crouched:
 		if can_sprint:
 			movement_speed = lerp(movement_speed, WALK_SPEED_BASE + SPRINT_SPEED_AMP, delta * 6)
 		else:
 			movement_speed = lerp(movement_speed, WALK_SPEED_BASE, delta * 6)
 
-	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-
-	if Input.is_action_just_pressed("move_dodge"):
-		if can_dodge:
-			velocity.x = lerp(velocity.x, direction.x * DODGE_AMP, delta * 9)
-			velocity.z = lerp(velocity.z, direction.z * DODGE_AMP, delta * 9)
-			can_dodge = false
-			await get_tree().create_timer(DODGE_COOLDOWN).timeout
-			can_dodge = true
-
 	if Input.is_action_pressed("move_crouch"):
 		movement_speed = lerp(movement_speed, WALK_SPEED_BASE - CROUCH_SPEED_AMP, delta * 3)
-		can_dodge = false
-		can_sprint = false
+		is_crouched = true
 		head.position.y = lerp(head.position.y, STANDING_HEIGHT - CROUCH_HEIGHT_AMP, delta * 9)
 		collider_standing.disabled = true
 		collider_crouched.disabled = false
 	else:
 		if not overhead_raycast.is_colliding():
 			movement_speed = lerp(movement_speed, WALK_SPEED_BASE, delta * 9)
-			can_dodge = true
-			can_sprint = true
+			is_crouched = false
 			head.position.y = lerp(head.position.y, STANDING_HEIGHT, delta * 3)
 			collider_crouched.disabled = true
 			collider_standing.disabled = false
 
 	if is_on_floor():
-		if direction:
-			velocity.x = lerp(velocity.x, direction.x * movement_speed, delta * 9.0)
-			velocity.z = lerp(velocity.z, direction.z * movement_speed, delta * 9.0)
+		if movement_direction:
+			velocity.x = lerp(velocity.x, movement_direction.x * movement_speed, delta * 9.0)
+			velocity.z = lerp(velocity.z, movement_direction.z * movement_speed, delta * 9.0)
 		else:
-			velocity.x = lerp(velocity.x, direction.x * movement_speed, delta * 6.0)
-			velocity.z = lerp(velocity.z, direction.z * movement_speed, delta * 6.0)
+			velocity.x = lerp(velocity.x, movement_direction.x * movement_speed, delta * 6.0)
+			velocity.z = lerp(velocity.z, movement_direction.z * movement_speed, delta * 6.0)
 	else:
-		velocity.x = lerp(velocity.x, direction.x * movement_speed, delta * 6.0)
-		velocity.z = lerp(velocity.z, direction.z * movement_speed, delta * 6.0)
+		velocity.x = lerp(velocity.x, movement_direction.x * movement_speed, delta * 6.0)
+		velocity.z = lerp(velocity.z, movement_direction.z * movement_speed, delta * 6.0)
 
 	if Input.is_action_pressed("move_jump") and is_on_floor():
 		velocity.y = JUMP_AMP
@@ -96,6 +96,14 @@ func _physics_process(delta):
 
 	if is_on_floor():
 		can_doublejump = true
+
+	if Input.is_action_just_pressed("move_dodge") and not is_crouched:
+		if can_dodge:
+			can_dodge = false
+			velocity.x = lerp(velocity.x, movement_direction.x * DODGE_AMP, delta * 9)
+			velocity.z = lerp(velocity.z, movement_direction.z * DODGE_AMP, delta * 9)
+			await get_tree().create_timer(DODGE_COOLDOWN).timeout
+			can_dodge = true
 
 	hbob += delta * velocity.length()  * float(is_on_floor())
 	camera.transform.origin = _headbob(hbob)
